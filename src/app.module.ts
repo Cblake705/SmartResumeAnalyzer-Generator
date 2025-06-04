0b1ff16e65f8dc0093a8b5028dc6d5eed4ab4c5b
Christian Blake <christian.blake@cypherblk.com>
2025-06-04 15:55:45 -0700

    Initial commit: scaffold Nest + TypeORM + Users module

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule, // import your UsersModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
